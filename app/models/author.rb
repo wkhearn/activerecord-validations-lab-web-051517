2c2b33e072f1f609e1179dfb2882899b1e947e8d
wkhearn <wkhearn@gmail.com>
2017-06-12 10:58:40 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { is: 10 }
+  # validates :bio, length: { maximum: 500 }
+  # validates :password, length: { in: 6..20 }
+  # validates :registration_number, length: { is: 6 }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :is_clickbait
+
+
+  def is_clickbait
+    if !title.nil?
+      if !title.include?("Won't Believe"||"Secret"||"Top"||"Guess")
+        errors.add(:is_clickbait, "Not clickbait-y enough!")
+      end
+    end
+  end
 end
